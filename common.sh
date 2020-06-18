df200b0af0e349904158ad2aaf50d90cb0ac53a7
Li Lin <guru.lin@gmail.com>
2020-06-18 21:44:11 +0800

    all: move common logic into a shared script

---
+++ common.sh
@@ -0,0 +1,31 @@
+#
+# utilities functions shared by various scripts
+#
+
+#
+# determine the the ingress host:port of the services
+#
+# use load balancer external IP when running in GKE
+# use service nodeport when running with minikube
+#
+function get_ingress_addr 
+{
+    CTX=$(kubectl config current-context)
+    case "$CTX" in
+
+        "gke1")
+            DEMOBANK_HOST=$(kubectl get svc istio-ingressgateway -n istio-system -o jsonpath="{.status.loadBalancer.ingress[*].ip}")
+            DEMOBANK_PORT=31400
+            ;;
+
+        "minikube")
+            DEMOBANK_HOST=$(minikube ip)
+            DEMOBANK_PORT=$(kubectl get svc istio-ingressgateway -n istio-system -o jsonpath="{.spec.ports[?(@.port=="31400")].nodePort}")
+            ;;
+
+        "")
+            echo No current kubectl context?...
+            ;;
+
+    esac
+}
